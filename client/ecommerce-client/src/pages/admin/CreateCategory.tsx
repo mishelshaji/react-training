4ac97670584684058718aac952ede25dfd8e1e9c
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2025-01-31 12:17:45 +0530

    Using yup for form validation

--- client/ecommerce-client/src/pages/admin/CreateCategory.tsx
+++ client/ecommerce-client/src/pages/admin/CreateCategory.tsx
@@ -3,21 +3,43 @@ import {useEffect, useState} from "react";
 import httpClient from "../../common/httpClient.ts";
 import Category from "../../common/types/category.ts";
 import {useForm} from "react-hook-form";
+import * as Yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
 
 interface FormFields {
     name: string;
     description: string;
 }
 
+const validationSchema = Yup.object({
+    name: Yup.string().required('Name is required')
+        .min(3, 'Name must be at least 3 characters')
+        .max(25, 'Name must be at most 25 characters'),
+    description: Yup.string().required('Description is required')
+        .min(3, 'Description must be at least 3 characters')
+        .max(255, 'Description must be at most 255 characters'),
+})
+
 function CreateCategory(){
     const {id} = useParams<{id: string}>();
     const [category, setCategory] = useState<Category | null>(null);
-    const {register, handleSubmit, formState} = useForm<FormFields>();
+    const {register, handleSubmit, formState, reset} = useForm<FormFields>({
+        disabled: false,
+        mode: 'all',
+        resolver: yupResolver(validationSchema)
+    });
+
 
     useEffect(()=>{
         if(id){
             httpClient.get<Category>(`/category/${id}`)
-                .then(response => setCategory(response.data))
+                .then(response => {
+                    setCategory(response.data);
+                    reset({
+                        name: response.data.name,
+                        description: response.data.description
+                    });
+                })
         }
     },[])
 
@@ -36,17 +58,7 @@ function CreateCategory(){
                     <label>Name</label>
                     <input type="text"
                            className={'border border-gray-400 focus:outline-2 focus:outline-indigo-200 p-2 w-full rounded'}
-                           {...register('name', {
-                               required: 'Name is required',
-                               minLength: {
-                                   value: 3,
-                                   message: 'Name must be at least 3 characters'
-                               },
-                               maxLength: {
-                                   value: 25,
-                                   message: 'Name must be at most 25 characters'
-                               },
-                           })}
+                           {...register('name')}
                            value={category?.name}
                     />
 
@@ -57,17 +69,7 @@ function CreateCategory(){
                     <label>Description</label>
                     <textarea rows={3}
                               className={'border border-gray-400 focus:outline-2 focus:outline-indigo-200 p-2 w-full rounded'}
-                              {...register('description', {
-                                  required: 'Description is required',
-                                  minLength: {
-                                      value: 3,
-                                      message: 'Description must be at least 3 characters'
-                                  },
-                                  maxLength: {
-                                      value: 255,
-                                      message: 'Description must be at most 255 characters'
-                                  },
-                              })}
+                              {...register('description')}
                               value={category?.description}></textarea>
 
                     {formState.errors.description && <p className="text-red-600">{formState.errors.description.message}</p>}
