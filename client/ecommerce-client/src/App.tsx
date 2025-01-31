c164bb756d80b4cda99d935e13270fba7c58444b
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2025-01-31 10:58:15 +0530

    Updated CreateCategory component to use React hook form

--- client/ecommerce-client/src/App.tsx
+++ client/ecommerce-client/src/App.tsx
@@ -1,9 +1,44 @@
+import {SubmitErrorHandler, SubmitHandler, useForm} from "react-hook-form";
+
 function App() {
+
+    interface FormValues{
+        email: string;
+        password: string;
+    }
+
+    const {register, handleSubmit, formState} = useForm<FormValues>();
+
+    const handleOnSubmit: SubmitHandler<FormValues> = (data) => {
+        console.log(data);
+        console.log(formState.errors);
+    }
+
+    const handleOnFormSubmitError: SubmitErrorHandler<FormValues> = (error) => {
+        console.log(error);
+    }
+
     return (
         <>
-                <div className="container">
-                        <h1 className="text-4xl font-bold">Category List</h1>
+            <form onSubmit={handleSubmit(handleOnSubmit, handleOnFormSubmitError)}>
+                <div>
+                    <label>Email</label>
+                    <input type="email"
+                           {...register('email')}
+                        className={'border border-gray-800 focus:outline-2 focus:outline-indigo-200 p-1 rounded'}/>
+                    {formState.errors.email && <p className="text-red-600">{formState.errors.email.message}</p>}
                 </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password"
+                           {...register('password', {required: 'Password is required'})}
+                           className={'border border-gray-800 focus:outline-2 focus:outline-indigo-200 p-1 rounded'}/>
+                    {formState.errors.password && <p className="text-red-600">{formState.errors.password.message}</p>}
+
+                </div>
+
+                <button type="submit">Submit</button>
+            </form>
         </>
     )
 }
--- client/ecommerce-client/src/pages/admin/CreateCategory.tsx
+++ client/ecommerce-client/src/pages/admin/CreateCategory.tsx
@@ -1,12 +1,18 @@
-import Button from "../../components/Button.tsx";
 import {useParams} from "react-router";
 import {useEffect, useState} from "react";
 import httpClient from "../../common/httpClient.ts";
 import Category from "../../common/types/category.ts";
+import {useForm} from "react-hook-form";
+
+interface FormFields {
+    name: string;
+    description: string;
+}
 
 function CreateCategory(){
     const {id} = useParams<{id: string}>();
     const [category, setCategory] = useState<Category | null>(null);
+    const {register, handleSubmit, formState} = useForm<FormFields>();
 
     useEffect(()=>{
         if(id){
@@ -15,31 +21,60 @@ function CreateCategory(){
         }
     },[])
 
+    const handleOnSubmit = (data: FormFields) => {
+        console.log(data);
+    }
+
     return(
         <div className={'container mx-auto px-4'}>
             <h1 className={'text-4xl font-bold text-center'}>
                 {id ? 'Update' : 'Create'} Category
             </h1>
 
-            <form className={'mt-10'}>
+            <form className={'mt-10'} onSubmit={handleSubmit(handleOnSubmit)}>
                 <div>
                     <label>Name</label>
                     <input type="text"
                            className={'border border-gray-400 focus:outline-2 focus:outline-indigo-200 p-2 w-full rounded'}
-                           required
+                           {...register('name', {
+                               required: 'Name is required',
+                               minLength: {
+                                   value: 3,
+                                   message: 'Name must be at least 3 characters'
+                               },
+                               maxLength: {
+                                   value: 25,
+                                   message: 'Name must be at most 25 characters'
+                               },
+                           })}
                            value={category?.name}
                     />
+
+                    {formState.errors.name && <p className="text-red-600">{formState.errors.name.message}</p>}
                 </div>
 
                 <div>
                     <label>Description</label>
                     <textarea rows={3}
                               className={'border border-gray-400 focus:outline-2 focus:outline-indigo-200 p-2 w-full rounded'}
-                              required value={category?.description}></textarea>
+                              {...register('description', {
+                                  required: 'Description is required',
+                                  minLength: {
+                                      value: 3,
+                                      message: 'Description must be at least 3 characters'
+                                  },
+                                  maxLength: {
+                                      value: 255,
+                                      message: 'Description must be at most 255 characters'
+                                  },
+                              })}
+                              value={category?.description}></textarea>
+
+                    {formState.errors.description && <p className="text-red-600">{formState.errors.description.message}</p>}
                 </div>
 
                 <div className={'flex justify-end'}>
-                    <Button type="submit" content="Save" severity="primary"/>
+                    <button type="submit">Save</button>
                 </div>
             </form>
         </div>
